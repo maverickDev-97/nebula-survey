ae4083bdd0fb633965d9e3d6bf757e32a0e4a025
Dmytro <dmitriy.pryshliak@gmail.com>
2025-03-04 21:41:44 +0200

    Hide back button on the first question

--- src/app/components/Header/BackButton.tsx
+++ src/app/components/Header/BackButton.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { Icons } from "../Icons/Icons";
 
 import styles from "./header.module.css";
 
 export const BackButton = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
+  if (/\bq1\b/.test(pathname)) {
+    return null;
+  }
 
   return (
     <button className={styles.iconWrapper} onClick={() => router.back()}>
